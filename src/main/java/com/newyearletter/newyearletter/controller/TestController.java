7c971d2d887919d9f977d748ebe184928e8c330b
sujin-park0607 <goweg0607@gmail.com>
2023-07-18 01:50:07 +0900

    feat: CICD 확인 테스트 Controller 구현

---
+++ src/main/java/com/newyearletter/newyearletter/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.newyearletter.newyearletter.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+@RequiredArgsConstructor
+@Slf4j
+public class TestController {
+    @GetMapping
+    public String test(){
+        return "CICD 성공";
+    }
+}
